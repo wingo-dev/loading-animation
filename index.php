720bef0ba52af5738929f15feab4cb307b2494a1
= <rukshanlancer@gmail.com>
2023-03-10 02:00:53 +0800

    change loading animation

--- index.php
+++ index.php
@@ -4,8 +4,38 @@
 <head>
     <title>Loading Animation</title>
     <style>
-        /* CSS animation for the loading spinner */
-        @keyframes spin {
+        .loader {
+            position: fixed;
+            z-index: 9999;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            /* make the background color darker and more transparent */
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        .loader:before {
+            content: "";
+            box-sizing: border-box;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 60px;
+            height: 60px;
+            margin-top: -30px;
+            margin-left: -30px;
+            border-radius: 50%;
+            border: 6px solid #fff;
+            border-color: #fff transparent #fff transparent;
+            animation: loader 1.2s linear infinite;
+        }
+
+        @keyframes loader {
             0% {
                 transform: rotate(0deg);
             }
@@ -14,31 +44,12 @@
                 transform: rotate(360deg);
             }
         }
-
-        .spinner {
-            display: block;
-            margin: 0 auto;
-            border: 4px solid rgba(0, 0, 0, 0.1);
-            border-top-color: #00bcd4;
-            border-radius: 50%;
-            width: 50px;
-            height: 50px;
-            animation: spin 1s linear infinite;
-            z-index: 9999;
-            position: fixed;
-            top: 50%;
-            left: 50%;
-            margin-top: -25px;
-            margin-left: -25px;
-            background-color: rgba(255, 255, 255, 0.5);
-            /* Transparent background */
-        }
     </style>
     <script>
         function showLoadingAnimation() {
             // Show the loading animation
             var spinner = document.createElement('div');
-            spinner.className = 'spinner';
+            spinner.className = 'loader';
             document.body.appendChild(spinner);
         }
     </script>
